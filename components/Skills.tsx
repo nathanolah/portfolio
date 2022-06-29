b60bece847845e0359c273a347f1e5f642c93672
Nathan Olah <nolah@myseneca.ca>
2022-06-28 22:07:57 -0400

    updated pages

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { useTheme } from "next-themes";
 import SkillsItem from "./SkillsItem";
+import typescriptImg from "../public/assets/skills/typescript-logo.png";
+import javascriptImg from "../public/assets/skills/javascript-logo.png";
+import graphqlImg from "../public/assets/skills/graphql-logo.png";
+import cppImg from "../public/assets/skills/cpp-logo.png";
+import cImg from "../public/assets/skills/c-logo.png";
+import javaImg from "../public/assets/skills/java-logo.png";
+import goImg from "../public/assets/skills/golang-logo.png";
+import csharpImg from "../public/assets/skills/csharp-logo.png";
+import htmlImg from "../public/assets/skills/html-logo.png";
+import cssImg from "../public/assets/skills/css-logo.png";
+import nodejsImg from "../public/assets/skills/nodejs-logo.png";
+import apolloImg from "../public/assets/skills/apollo-logo.png";
+import linuxImg from "../public/assets/skills/linux-logo.png";
+import postgreSqlImg from "../public/assets/skills/postgreSQL-logo.png";
+import mongodbImg from "../public/assets/skills/mongodb-logo.png";
+import dockerImg from "../public/assets/skills/docker-logo.png";
+import redisImg from "../public/assets/skills/redis-logo.png";
+import reactImg from "../public/assets/skills/react-logo.png";
+import tailwindcssImg from "../public/assets/skills/tailwindcss-logo.png";
+import awsWhiteImg from "../public/assets/skills/AWS-logo-white.png";
+import awsDarkImg from "../public/assets/skills/AWS-logo-dark.png";
+import herokuImg from "../public/assets/skills/heroku-logo.png";
+import gitImg from "../public/assets/skills/git-logo.png";
+import vimImg from "../public/assets/skills/vim-logo.png";
+import angularImg from "../public/assets/skills/angular-logo.png";
+import azureImg from "../public/assets/skills/azure-logo.png";
+import jenkinsImg from "../public/assets/skills/jenkins-logo.png";
 
 const Skills = () => {
   const { theme } = useTheme();
@@ -20,52 +47,44 @@ const Skills = () => {
         {/* Grid */}
         <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-7">
           <SkillsItem
-            imageSrc="/../public/assets/skills/typescript-logo.png"
+            imageSrc={typescriptImg}
             name="TypeScript"
             bgGradient="from-[#88caf7]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/javascript-logo.png"
+            imageSrc={javascriptImg}
             name="JavaScript"
             bgGradient="from-[#f5e798]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/graphql-logo.png"
+            imageSrc={graphqlImg}
             name="GraphQL"
             bgGradient="from-[#f89ad9]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/cpp-logo.png"
+            imageSrc={cppImg}
             name="C++"
             bgGradient="from-[#6db1f1]"
           />
+          <SkillsItem imageSrc={cImg} name="C" bgGradient="from-[#7d8df5]" />
           <SkillsItem
-            imageSrc="/../public/assets/skills/c-logo.png"
-            name="C"
-            bgGradient="from-[#7d8df5]"
-          />
-          <SkillsItem
-            imageSrc="/../public/assets/skills/java-logo.png"
+            imageSrc={javaImg}
             name="Java"
             bgGradient="from-[#87b2cc]"
           />
+          <SkillsItem imageSrc={goImg} name="Go" bgGradient="from-[#86e0f7]" />
           <SkillsItem
-            imageSrc="/../public/assets/skills/golang-logo.png"
-            name="Go"
-            bgGradient="from-[#86e0f7]"
-          />
-          <SkillsItem
-            imageSrc="/../public/assets/skills/csharp-logo.png"
+            imageSrc={csharpImg}
             name="C#"
             bgGradient="from-[#e37af3]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/html-logo.png"
+            imageSrc={htmlImg}
             name="HTML"
             bgGradient="from-[#e49883]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/css-logo.png"
+            imageSrc={cssImg}
             name="CSS"
             bgGradient="from-[#7dade4]"
           />
@@ -73,86 +92,82 @@ const Skills = () => {
         <h2 className="py-7 font-semibold">Technologies</h2>
         <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-8">
           <SkillsItem
-            imageSrc="/../public/assets/skills/nodejs-logo.png"
+            imageSrc={nodejsImg}
             name="NodeJS"
             bgGradient="from-[#74ac72]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/apollo-logo.png"
+            imageSrc={apolloImg}
             name="Apollo"
             bgGradient="from-[#ab9aec]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/linux-logo.png"
+            imageSrc={linuxImg}
             name="Linux"
             bgGradient="from-[#fce589]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/postgreSQL-logo.png"
+            imageSrc={postgreSqlImg}
             name="PostgreSQL"
             bgGradient="from-[#89baeb]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/mongodb-logo.png"
+            imageSrc={mongodbImg}
             name="MongoDB"
             bgGradient="from-[#9ce48f]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/docker-logo.png"
+            imageSrc={dockerImg}
             name="Docker"
             bgGradient="from-[#89ccff]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/redis-logo.png"
+            imageSrc={redisImg}
             name="Redis"
             bgGradient="from-[#f39690]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/react-logo.png"
+            imageSrc={reactImg}
             name="React"
             bgGradient="from-[#9eeefc]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/tailwindcss-logo.png"
+            imageSrc={tailwindcssImg}
             name="Tailwind CSS"
             bgGradient="from-[#85e0f0]"
           />
           <SkillsItem
-            imageSrc={
-              darkModeImg
-                ? "/../public/assets/skills/AWS-logo-white.png"
-                : "/../public/assets/skills/AWS-logo-dark.png"
-            }
+            imageSrc={darkModeImg ? awsWhiteImg : awsDarkImg}
             name="AWS"
             bgGradient="from-[#92acda]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/heroku-logo.png"
+            imageSrc={herokuImg}
             name="Heroku"
             bgGradient="from-[#b2aeeb]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/git-logo.png"
+            imageSrc={gitImg}
             name="Git"
             bgGradient="from-[#f19f90]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/vim-logo.png"
+            imageSrc={vimImg}
             name="Vim"
             bgGradient="from-[#74cf92]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/angular-logo.png"
+            imageSrc={angularImg}
             name="Angular"
             bgGradient="from-[#fa8c88]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/azure-logo.png"
+            imageSrc={azureImg}
             name="Azure"
             bgGradient="from-[#88cff5]"
           />
           <SkillsItem
-            imageSrc="/../public/assets/skills/jenkins-logo.png"
+            imageSrc={jenkinsImg}
             name="Jenkins"
             bgGradient="from-[#7898ac]"
           />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider enableSystem={true} attribute="class">
       <Head>
         <title>Nathan | Software Developer</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
+        ></link>
       </Head>
       <Navbar />
       <Component {...pageProps} />
